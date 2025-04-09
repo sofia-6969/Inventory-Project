2cd9b50cf25727b4adba540f0cc49e5fc4963263
Sofia <dannavelandia6969@gmail.com>
2025-04-09 15:21:00 -0500

    se agrego un for para que se puedan ingresar datos

--- Inventory.java
+++ Inventory.java
@@ -5,14 +5,30 @@ public class Inventory {
     public static void main(String[] args) {
         Scanner input  = new Scanner(System.in);
 
-        final int longitudCategoria = categoria.length;
+        //int longitudCategoria = categorias.length;
         
-        String[] categorias = {"Alimentos", "Limpieza"}
+        String[] categorias = {"Alimentos", "Limpieza"};
 
         String[][] nombres = new String[categorias.length][3];
         int[][] cantidades = new int[categorias.length][3];
         double[][] precios = new double[categorias.length][3];
 
-        
+        for (int i = 0; i < categorias.length; i++) {
+            System.out.println("Categorias: " + categorias[i]);
+            for (int j = 0; j < 3; j++) {
+                System.out.println("ingrese el nombre del producto " + (j + 1) + ": ");
+                nombres[i][j] = input.nextLine();
+                
+                System.out.println("ingrese la cantidad del producto: ");
+                cantidades[i][j] = input.nextInt();
+                input.nextLine();
+
+                System.out.println("ingrese el precio por unidad ");
+                precios[i][j] = input.nextDouble();
+                input.nextLine();
+            }
+        }
+
+        input.close();
     }
 }
