7bfc6a6f888d926ed562d5d9c13784a90690b43b
Sofia <dannavelandia6969@gmail.com>
2025-04-09 14:50:32 -0500

    se definio categorias y se hizo la matriz para almacenar datos

--- Inventory.java
+++ Inventory.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Inventory {
 
     public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+
+        final int longitudCategoria = categoria.length;
+        
+        String[] categorias = {"Alimentos", "Limpieza"}
+
+        String[][] nombres = new String[categorias.length][3];
+        int[][] cantidades = new int[categorias.length][3];
+        double[][] precios = new double[categorias.length][3];
+
         
     }
 }
